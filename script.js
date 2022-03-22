d49e5840d3a58db01d9b34ba51fdda13ceeb7845
Yasser <namezyasser3@gmail.com>
2022-03-22 19:27:19 +0100

    end of my clock project

---
+++ script.js
@@ -0,0 +1,21 @@
+let s = document.querySelector("#sec")
+let m = document.querySelector("#min")
+let h = document.querySelector("#hour")
+
+function dat() {
+    let date = new Date()
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+    if (hr === 0) {
+        hr = 12;
+    }
+    if (hr > 12) {
+        hr = hr - 12;
+    }
+    h.setAttribute('transform', `rotate(${hr*30})`)
+    m.setAttribute('transform', `rotate(${min*6})`)
+    s.setAttribute('transform', `rotate(${sec*6})`)
+}
+
+let v = setInterval(dat, 1000)
